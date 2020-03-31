a2fa4902d1e94e04849d2c3a216a4e3e6dc1cf96
Michelle Davies Thalakottur <44194090+michelledaviest@users.noreply.github.com>
2020-03-31 10:49:59 +0530

    Added Kaprekar plot

--- kaprekars 6174/kaprekars.R
+++ kaprekars 6174/kaprekars.R
@@ -21,6 +21,22 @@ kaprekars_steps <- function(num,step){
   kaprekars_steps(num_d-num_a,step+1)
 }
 
+pad_num <- function(num){
+  if(as.integer(log(as.integer(num),10)+1) == 4) return(as.integer(num))
+  else pad_num(paste(num,0,sep=""))
+}
+
+mat = matrix(0, nrow=99, ncol=99)
+for(i in 1:99){
+  for(j in 1:99){
+    mat[i,j] = kaprekars_steps(pad_num(paste(i,j,sep="")),0)
+  }
+}
+
+image(1:99, 1:99, mat, asp = 1, axes = F, xlab ='', ylab ='')
+
 cat(kaprekars_steps(9218,0),"\n")
 cat(kaprekars_steps(9090,0),"\n")
 cat(kaprekars_steps(2222,0),"\n")
+cat(pad_num(as.character(89)),"\n")
+cat(pad_num(as.character(195)),"\n")
