7e76d64ede8e72a4360da04a3a8a6b941ea4fb4c
Michelle Davies Thalakottur <44194090+michelledaviest@users.noreply.github.com>
2020-04-02 15:20:29 +0530

    holey plot added

--- hol(e)y poly/holey-doughnut.R
+++ hol(e)y poly/holey-doughnut.R
@@ -1,21 +1,26 @@
-library(polynom)
+library(plotrix)
+library(RColorBrewer)
 
-plot.new ()
-#op <- par ( mar = rep ( 0, 4 ) )
-plot.window ( asp = 1, xlim = c( -2, +2 ), ylim = c( -2, +2 ) )
-abline ( h = 0, v = 0, col = 'gray ', lwd = 0.5 )
-symbols ( rep ( 0, 2 ), rep ( 0, 2 ), circles = c( 1, 2 ),
-          inches = F, fg = 'gray ', lwd = 0.5 , add = T )
+plot.new()
+op <- par(mar =rep ( 0, 4 ) )
+plot.window(asp = 1, xlim = c(-2,+2 ), ylim = c(-2,+2 ))
+abline( h = 0, v = 0, col = 'gray', lwd = 0.5 )
+draw.circle(0,0,1, border="gray")
+draw.circle(0,0,2, border="gray")
+
+cols = brewer.pal(n = 8, name = "RdBu")
+k = length(col)
 
 for(m in 11:1){
   roots = c(0)
   for(i in 0:(2**(m+1)-1)){
     #cat(i,"\n")
-    z = as.integer(intToBits(i))[1:(m+1)]
-    z[z==0] <- -1
+    temp = as.integer(intToBits(i))[1:(m+1)]
+    temp[temp==0] <- -1
     #append(roots, solve(polynomial(z)), after=length(roots))
-    roots <- cbind( roots , polyroot( z ) )
+    roots <- cbind(roots,polyroot(temp))
   }
-  points(x=Re(roots), y=Im(roots), pch=16, col="blue", cex = 0.001)
+  points(x=Re(roots), y=Im(roots), pch=16, col=cols[k], cex = 0.001)
+  k=k-1
 }
 
